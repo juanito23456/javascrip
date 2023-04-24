a051b5e64b0e1bcf1b40af37efc0b37f47d2b10a
juani <juanpintoparodi@gmail.com>
2023-04-24 15:52:12 -0300

    changes

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("hola coder"); 
+
+var edad = 35;
+var edad = 36;
+
+let nombreUsuario = "juan ignacio";
+nombreUsuario = "maria";
+
+console.log ("la edad del usuario/a es : " + edad);
+console.log ("el nombre de usuario/a es : " + nombreUsuario);
+ 
+const EMAIL = "hola@chau.com";
+const IVA = 21;
+console.log("IVA : " + IVA);
+console.log("el email del usuario es : " + EMAIL);
+
+let nombreIngresado = prompt("ingrese su nombre : ");
+alert("el nombre ingresado fue : " + nombreIngresado);
