2be4e1ccd308f2beb7f9a041192958f114620299
juani <juanpintoparodi@gmail.com>
2023-04-24 18:56:46 -0300

    latest

--- main.js
+++ main.js
@@ -1,17 +1,126 @@
 
-var edad = 35;
-var edad = 36;
+//var edad = 35;
+//var edad = 36;
 
-let nombreUsuario = "juan ignacio";
-nombreUsuario = "maria";
+//let nombreUsuario = "juan ignacio";
+//nombreUsuario = "maria";
 
-console.log ("la edad del usuario/a es : " + edad);
-console.log ("el nombre de usuario/a es : " + nombreUsuario);
+//console.log ("la edad del usuario/a es : " + edad);
+//console.log ("el nombre de usuario/a es : " + nombreUsuario);
  
-const EMAIL = "hola@chau.com";
-const IVA = 21;
-console.log("IVA : " + IVA);
-console.log("el email del usuario es : " + EMAIL);
+//const EMAIL = "hola@chau.com";
+//const IVA = 21;
+//console.log("IVA : " + IVA);
+//console.log("el email del usuario es : " + EMAIL);
 
-let nombreIngresado = prompt("ingrese su nombre : ");
-alert("el nombre ingresado fue : " + nombreIngresado);
+//let nombreIngresado = prompt("ingrese su nombre : ");
+//alert("el nombre ingresado fue : " + nombreIngresado);
+
+//operaciones basicas: + - * /
+
+//let numero1 = 10;
+//let numero2 = 20;
+//const NUMEROC = 15; 
+//+ es concatenar texto o string con variable
+//suma 
+
+//let resultadoSuma = numero1 + numero2;
+//console.log("el resultado de la suma es :" + resultadoSuma);
+
+//let resultadoResta = numero1 - NUMEROC;
+//console.log("el resultado de la resta es:" + resultadoResta);
+
+//let resultadoMulti = numero1 * numero2;
+//console.log("el resultado de la multi es:" + resultadoMulti);
+
+//let resultadoDiv = numero1 / numero2;
+//console.log("el resultado de la div es:" + resultadoDiv);
+
+//cadena de carateres
+
+//let textoA = "Coder";
+//let textoB = "House";
+
+//const BLANCO = " ";
+
+//let resultado = textoA + BLANCO + textoB;
+//alert(resultado);
+
+//console.log ( typeof textoA)
+
+//le pedimos datos al usuario, suma en enteros
+
+//let numero3 = parseInt (prompt("ingrese un numero:"));
+//let numero4 = parseInt (prompt("ingrese otro numero:"));
+//let resultadoOperacion = numero3 + numero4; 
+//alert("el resultado de la operacion es :" + resultadoOperacion);
+//console.log ("resultado operacion:"+ resultadoOperacion);
+
+//parseo con number, decimales
+
+
+//let numero5 = Number (prompt("ingrese un numero:"));
+//let numero6 = Number (prompt("ingrese otro numero:"));
+//let resultadoOperacion2 = numero5 + numero6; 
+//alert("el resultado de la operacion 2 es :" + resultadoOperacion2);
+//console.log ("resultado operacion 2:"+ resultadoOperacion2);
+
+//parseFloat, redondea enteros
+
+//let numero7 = parseFloat (prompt("ingrese un numero:"));
+//let numero8 = parseFloat (prompt("ingrese otro numero:"));
+//let resultadoOperacion3 = numero7 + numero8; 
+//alert("el resultado de la operacion 3 es :" + resultadoOperacion3);
+//console.log ("resultado operacion 3:"+ resultadoOperacion3);
+
+//estructuras de control
+// true or false
+
+//if(true){
+  //  console.log("vas a ver este mensaje");
+//}
+//si fuera false no se mostraria el mensaje
+// if evalua la condicion dentro del parentesis y ejecuta o no
+
+//operacion de equivalencia
+
+//let unNumero = 5;
+
+//if(unNumero == 4){
+  //  console.log("el numero es igual a 5");
+//} else{ console.log("el numero es distinto de 5");}
+
+//el sino o el camino del falso se muestra con el else 
+
+//let nombreUsuario = prompt("ingrese su usuario: ");
+
+//if (nombreUsuario == ""){
+  //  alert("no ingresaste nombre de usuario");
+//} else{ alert("el nombre de usuario es : " + nombreUsuario)};
+
+//IF anidados
+
+//let precioProducto = Number (prompt("ingrese el precio del producto:"));
+
+//if (precioProducto < 20){
+  //  console.log("el precio del producto es menor a 20 pesos ");
+//} else if (precioProducto < 50){
+  //  console.log("el precio ingresado es menor a 50 pesos");
+//} else if (precioProducto <100){
+  //  console.log("el precio ingresado es menor a 100");
+//} else {
+  //  console.log("el precio ingresado es mayor a 100 pesos");
+//}
+
+//variables booleanas
+
+//let edad = Number (prompt("ingrese su edad"));
+//const ESMAYOR = (edad >= 18);
+
+//if (ESMAYOR){
+//    alert("BIENVENID@ AL SITIO");
+//} else {alert("debes ser mayor de edad para ingresar")}
+
+//operadores logicos
+
+
